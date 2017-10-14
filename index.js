969a5c2dbb4e603e21651c155cd32a241bf6bd88
Kevin Quinn <kpquinn2@wisc.edu>
2017-10-14 11:18:21 -0500

    Rename render

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var render = require('./render');
+var fbApp = require('./firebase');
 
 var signup = document.getElementById('signUpBtn');
 var signin = document.getElementById('signInBtn');
@@ -10,7 +10,7 @@ var dbButton = document.getElementById('databaseBtn');
 signup.addEventListener('click', function() {
   console.log('signup clicked');
 
-  render.firebase.auth().createUserWithEmailAndPassword(email.value, password.value).catch(function(err) {
+  fbApp.firebase.auth().createUserWithEmailAndPassword(email.value, password.value).catch(function(err) {
     if (err != null) {
       console.log(err.message);
       return;
@@ -21,7 +21,7 @@ signup.addEventListener('click', function() {
 signin.addEventListener('click', function() {
   console.log('signin clicked');
 
-  render.firebase.auth().signInWithEmailAndPassword(email.value, password.value).then( function() {
+  fbApp.firebase.auth().signInWithEmailAndPassword(email.value, password.value).then( function() {
     document.location.href = 'mainWindow.html';
   }).catch(function(err) {
     if (err != null) {
