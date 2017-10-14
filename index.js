1df6f715364cabf653b411078efbc27c73c02991
Austin Meyer <frizzkitten@gmail.com>
2017-10-14 04:35:19 -0400

    create the main window

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
  const url = require('url');
  const path = require('path');
 
- 
+ const {app, Browser} = electron;
+
+let mainWindow;
+
+app.on('ready', function() {
+  // create new window
+  mainWindow = new BrowserWindow({});
+})
