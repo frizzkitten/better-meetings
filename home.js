2c34f7d2b2f74edda032baa1049c28c6787c0111
Austin Meyer <frizzkitten@gmail.com>
2017-10-14 23:36:06 -0400

    populates groups with fake data

--- home.js
+++ home.js
@@ -10,6 +10,8 @@ var signout = document.getElementById('signout');
 var dbBtn = document.getElementById('databaseBtn');
 var database = base_module.fbApp.database();
 
+let accordion = $('#accordion');
+
 
 //fetching functions
 function fetchAllGroups() {
@@ -18,7 +20,7 @@ function fetchAllGroups() {
     {
       "Name": "Scrum Meetings",
       "Key": 12345,
-      "MeetingKeyNames: " : [
+      "MeetingKeyNames" : [
         { "Key": 9999, "Name": "Sprint 1 Day 1" },
         { "Key": 8888, "Name": "Sprint 1 Day 2" },
         { "Key": 7777, "Name": "Sprint 1 Day 3" }
@@ -27,7 +29,7 @@ function fetchAllGroups() {
     {
       "Name": "Misc",
       "Key": 98765,
-      "MeetingKeyNames: " : [
+      "MeetingKeyNames" : [
         { "Key": 9999, "Name": "The Rat Problem" },
         { "Key": 8888, "Name": "How Should We Spell Hannukah???" },
         { "Key": 7777, "Name": "Dude, Where's My Car?" }
@@ -63,14 +65,52 @@ function fetchMeetingFromKey(key) {
 
 // end fetching functions
 
-// uploading functions
+// putting into DOM
+function putGroupsInDOM() {
+  let groups = fetchAllGroups();
+  console.log(groups);
+
+  for (let groupIdx = 0; groupIdx < groups.length; groupIdx++) {
+    let group = groups[groupIdx];
+
+    let groupHTML = "";
+
+    let groupPreNameHTML = "<div class='card'>" +
+      "<div class='card-header' role='tab' id='heading" + group.Key + "'>" +
+        "<h5 class='mb-0'>" +
+          "<a data-toggle='collapse' href='#collapse" + group.Key + "' aria-expanded='true' aria-controls='collapse" + group.Key + "'>"
+
+    let groupMiddleHTML = "</a></h5></div>" +
+        "<div id='collapse" + group.Key + "' class='collapse' role='tabpanel' aria-labelledby='heading" + group.Key + "' data-parent='#accordion'>" +
+          "<div class='card-body'><div class='row'><div class='col-4' style='max-width:100%; flex:0 0 100%'>" +
+                "<div class='list-group' id='list-tab' role='tablist'>"
+
+    let currGroupMeetingsHTML = "";
+
+    console.log(group);
+
+    for (let meetingIdx = 0; meetingIdx < group.MeetingKeyNames.length; meetingIdx++) {
+      let meeting = group.MeetingKeyNames[meetingIdx];
+      let meetingHTML = "<a class='list-group-item list-group-item-action active' id='list-" + meeting.Key + "-list' data-toggle='list' href='#list-" + meeting.Key + "' role='tab' aria-controls='" + meeting.Key + "'>" + meeting.Name + "</a>";
+      currGroupMeetingsHTML = currGroupMeetingsHTML + meetingHTML;
+    }
+
+    let groupEndHTML = "</div></div></div></div></div></div>"
+
+    // add the group to the list
+    groupHTML= groupPreNameHTML + group.Name + groupMiddleHTML + currGroupMeetingsHTML + groupEndHTML;
+
+    accordion.append(groupHTML);
+  }
+}
 
 
 
+putGroupsInDOM();
+//putMeetingsInDOM();
 
 
-
-// end uploading functions
+// end putting into DOM
 
 function addMeeting(name) {
   var key = database.ref().child('meetings').push().key;
@@ -179,21 +219,21 @@ signout.addEventListener('click', function() {
 });
 
 
-dbBtn.addEventListener('click', function() {
-  var meetingKey = addMeeting('meeting');
-  addMeeting('meeting2');
-  addMeeting('meeting3');
-  var meetings = database.ref(meetingsRef);
-  meetings.orderByChild('key').on('child_added', function(data) {
-    console.log(data.val().name + ' ' + data.val());
-  });
-
-  var groupKey = addGroup('FunTimes');
-  addMeetingToGroup(meetingKey, groupKey);
-  var tags = ['taco', 'burrito', 'girls'];
-  var date = '1010203';
-  var uids = ['123', '456'];
-  updateMeetingInfo(meetingKey, uids, date, tags);
-  addQuestion(meetingKey, 'question and answer', 'deails');
-  getMeetingQuestions(meetingKey);
-});
+// dbBtn.addEventListener('click', function() {
+//   var meetingKey = addMeeting('meeting');
+//   addMeeting('meeting2');
+//   addMeeting('meeting3');
+//   var meetings = database.ref(meetingsRef);
+//   meetings.orderByChild('key').on('child_added', function(data) {
+//     console.log(data.val().name + ' ' + data.val());
+//   });
+//
+//   var groupKey = addGroup('FunTimes');
+//   addMeetingToGroup(meetingKey, groupKey);
+//   var tags = ['taco', 'burrito', 'girls'];
+//   var date = '1010203';
+//   var uids = ['123', '456'];
+//   updateMeetingInfo(meetingKey, uids, date, tags);
+//   addQuestion(meetingKey, 'question and answer', 'deails');
+//   getMeetingQuestions(meetingKey);
+// });
