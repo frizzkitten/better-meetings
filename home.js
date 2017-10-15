8f9fc44141da48f2e6750b4d16cca97bd2e3e857
Austin Meyer <frizzkitten@gmail.com>
2017-10-15 01:08:20 -0400

    added all fake news

--- home.js
+++ home.js
@@ -11,31 +11,190 @@ var dbBtn = document.getElementById('databaseBtn');
 var database = base_module.fbApp.database();
 
 let accordion = $('#accordion');
+let navtabContent = $('#nav-tabContent');
+
+let groups = []
 
 
 //fetching functions
+
+
+let meeting1 = {
+  "Name": "Sprint 1 Day 1",
+  "Key": 9999,
+  "Members": [
+    { "Key": 1212, "Name": "Austin Meyer" },
+    { "Key": 2323, "Name": "Kevin Quinn" },
+    { "Key": 3434, "Name": "Joe McKee" }
+  ],
+  "Tags": [
+    { "Key": 1029, "Name": "Dev" },
+    { "Key": 1929, "Name": "Prof Dev" }
+  ],
+  "Questions": [
+    {
+      "Key": 110243363240,
+      "Question": "Problems?",
+      "Details": "Worried about profits next term."
+    },
+    {
+      "Key": 114578800,
+      "Question": "Questions for the boss?",
+      "Details": "What does the last quarter mean for our benefits?"
+    },
+  ]
+}
+let meeting2 = {
+  "Name": "Sprint 1 Day 2",
+  "Key": 9912345899,
+  "Members": [
+    { "Key": 1213457463452, "Name": "Remus Lupin" },
+    { "Key": 2334575323, "Name": "Kevin Quinn" },
+    { "Key": 323467456434, "Name": "Austin Klisch" }
+  ],
+  "Tags": [
+    { "Key": 10456729, "Name": "Routine" },
+    { "Key": 19262229, "Name": "Prof Dev" }
+  ],
+  "Questions": [
+    {
+      "Key": 110450987654,
+      "Question": "Problems?",
+      "Details": "None today."
+    },
+    {
+      "Key": 15610206456,
+      "Question": "Any new development ideas?",
+      "Details": "If we could have those reports by Thursday that'd be greaaaaat."
+    },
+  ]
+}
+let meeting3 = {
+  "Name": "Sprint 1 Day 3",
+  "Key": 99121267885899,
+  "Members": [
+    { "Key": 12134574623453452, "Name": "Harry Potter" },
+    { "Key": 23345723455323, "Name": "Draco Malfoy" },
+  ],
+  "Tags": [
+    { "Key": 104562345729, "Name": "Marketing" },
+    { "Key": 192622345229, "Name": "Dev" },
+    { "Key": 192622345229, "Name": "QA" }
+  ],
+  "Questions": [
+    {
+      "Key": 110455555,
+      "Question": "Problems?",
+      "Details": "Lots of them."
+    },
+    {
+      "Key": 123453450,
+      "Question": "What time is it?",
+      "Details": "Not the ideal one."
+    },
+  ]
+}
+let meeting4 = {
+  "Name": "Dude, Where's my Car?",
+  "Key": 9999,
+  "Members": [
+    { "Key": 1212, "Name": "Austin Meyer" },
+    { "Key": 2323, "Name": "Kevin Quinn" },
+    { "Key": 3434, "Name": "Joe McKee" }
+  ],
+  "Tags": [
+    { "Key": 1029, "Name": "Dev" },
+    { "Key": 1929, "Name": "Prof Dev" }
+  ],
+  "Questions": [
+    {
+      "Key": 110243363240,
+      "Question": "Problems?",
+      "Details": "Worried about profits next term."
+    },
+    {
+      "Key": 114578800,
+      "Question": "Questions for the boss?",
+      "Details": "What does the last quarter mean for our benefits?"
+    },
+  ]
+}
+let meeting5 = {
+  "Name": "Does Tina Even Lift?",
+  "Key": 99123234545899,
+  "Members": [
+    { "Key": 12123453457463452, "Name": "Ron Weasley" },
+    { "Key": 23342345575323, "Name": "Hermione Grainger" },
+    { "Key": 3234234567456434, "Name": "Lucius Malfoy" }
+  ],
+  "Tags": [
+    { "Key": 104562345729, "Name": "Routine" },
+    { "Key": 192623452229, "Name": "Dev" }
+  ],
+  "Questions": [
+    {
+      "Key": 110453450987654,
+      "Question": "How much longer do we need until we're in the black?",
+      "Details": "Between 10 days and 13 years."
+    },
+    {
+      "Key": 156102334506456,
+      "Question": "Any new development ideas?",
+      "Details": "Mars on the Moon 2065."
+    },
+  ]
+}
+let meeting6 = {
+  "Name": "The Implications of McDungle",
+  "Key": 991212673456885899,
+  "Members": [
+    { "Key": 12134534574623453452, "Name": "Cumberbatch Wiggins" },
+    { "Key": 233345645723455323, "Name": "Jernerthern Merclerber" },
+  ],
+  "Tags": [
+    { "Key": 1045623345729, "Name": "Marketing" },
+    { "Key": 19262234345229, "Name": "Dev" },
+    { "Key": 1926223345229, "Name": "QA" }
+  ],
+  "Questions": [
+    {
+      "Key": 1104523455555,
+      "Question": "How could we do better next time?",
+      "Details": "Better commmunication."
+    },
+    {
+      "Key": 1234543253450,
+      "Question": "Yellow Is the New Gray",
+      "Details": "Meh, Tournament of Chairs is better."
+    },
+  ]
+}
+
+
+
 function fetchAllGroups() {
   // fake data
-  return [
+  groups =  [
     {
       "Name": "Scrum Meetings",
       "Key": 12345,
       "MeetingKeyNames" : [
-        { "Key": 9999, "Name": "Sprint 1 Day 1" },
-        { "Key": 8888, "Name": "Sprint 1 Day 2" },
-        { "Key": 7777, "Name": "Sprint 1 Day 3" }
+        meeting1,
+        meeting2,
+        meeting3
       ]
     },
     {
       "Name": "Misc",
       "Key": 98765,
       "MeetingKeyNames" : [
-        { "Key": 9999, "Name": "The Rat Problem" },
-        { "Key": 8888, "Name": "How Should We Spell Hannukah???" },
-        { "Key": 7777, "Name": "Dude, Where's My Car?" }
+        meeting4,
+        meeting5,
+        meeting6
       ]
     }
   ];
+  return groups;
 }
 
 function fetchMeetingFromKey(key) {
@@ -46,7 +205,11 @@ function fetchMeetingFromKey(key) {
     "Members": [
       { "Key": 1212, "Name": "Austin Meyer" },
       { "Key": 2323, "Name": "Kevin Quinn" },
-      { "Key": 3434, "Name": "Joe McKee (RIP)" }
+      { "Key": 3434, "Name": "Joe McKee" }
+    ],
+    "Tags": [
+      { "Key": 1029, "Name": "Dev" },
+      { "Key": 1929, "Name": "Prof Dev" }
     ],
     "Questions": [
       {
@@ -67,7 +230,7 @@ function fetchMeetingFromKey(key) {
 
 // putting into DOM
 function putGroupsInDOM() {
-  let groups = fetchAllGroups();
+  groups = fetchAllGroups();
   console.log(groups);
 
   for (let groupIdx = 0; groupIdx < groups.length; groupIdx++) {
@@ -105,7 +268,19 @@ function putGroupsInDOM() {
 }
 
 function putMeetingsInDOM() {
+  insertHTML = "";
 
+  for (let groupIdx = 0; groupIdx < groups.length; groupIdx++) {
+    let group = groups[groupIdx];
+    for (let meetingIdx = 0; meetingIdx < group.MeetingKeyNames.length; meetingIdx++) {
+      let meeting = group.MeetingKeyNames[meetingIdx];
+
+      insertHTML = insertHTML + "<div class='tab-pane fade show active' id='list-" + meeting.Key + "' role='tabpanel' aria-labelledby='list-" + meeting.Key + "-list'>" +
+        "<h3 class='meeting-name'>" + meeting.Name + "</h3></div>";
+    }
+  }
+
+  navtabContent.append(insertHTML);
 }
 
 putGroupsInDOM();
